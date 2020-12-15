abfaa6038fa8940b28f923761214f9f7ee6acf02
Kyle Thousand <KAthousand@gmail.com>
2020-12-15 18:26:28 -0500

    added hover to work page

--- client/src/screens/Work/Work.jsx
+++ client/src/screens/Work/Work.jsx
@@ -1,43 +1,79 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router-dom";
 import "../Work/Work.css";
 import WorkCard from "../../components/WorkCard/WorkCard";
 
 function Work(props) {
-  const [hover, setHover] = useState({
-    one: false,
-    two: false,
-    three: false,
-  });
+  const [hoverOne, setHoverOne] = useState(false);
+  const [hoverTwo, setHoverTwo] = useState(false);
+  const [hoverThree, setHoverThree] = useState(false);
 
-  const handleHover = (e) => {
-    setHover(!hover);
-  };
   return (
     <div className="work-container">
       <WorkCard />
       <div className="work-content">
         <div
           className="blackjack"
-          name="one"
-          onMouseEnter={(e) => handleHover(e)}
-          onMouseLeave={handleHover}
+          onMouseEnter={() => setHoverOne(true)}
+          onMouseLeave={() => setHoverOne(false)}
         >
-          <div className={hover && "description"}></div>
+          {hoverOne && (
+            <div className="description">
+              <div className="description-text">
+                <h1>Blackjack</h1>
+                <p>
+                  Play a game of blackjack written completely in HTML, CSS, and
+                  vanilla JavaScript
+                </p>
+              </div>
+              <div className="link-container">
+                <Link to="">GitHub</Link>
+                <Link to="">Deploy</Link>
+              </div>
+            </div>
+          )}
         </div>
         <div
           className="paloma"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleHover}
+          onMouseEnter={() => setHoverTwo(true)}
+          onMouseLeave={() => setHoverTwo(false)}
         >
-          <div className={hover && "description"}></div>
+          {hoverTwo && (
+            <div className="description">
+              <div className="description-text">
+                <h1>Paloma</h1>
+                <p>
+                  A recipe database and search engine for classic cocktail
+                  recipes.
+                </p>
+              </div>
+              <div className="link-container">
+                <Link to="">GitHub</Link>
+                <Link to="">Deploy</Link>
+              </div>
+            </div>
+          )}
         </div>
         <div
           className="high-noon"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleHover}
+          onMouseEnter={() => setHoverThree(true)}
+          onMouseLeave={() => setHoverThree(false)}
         >
-          <div className={hover && "description"}></div>
+          {hoverThree && (
+            <div className="description">
+              <div className="description-text">
+                <h1>High Noon</h1>
+                <p>
+                  A Western shooting gallery game designed to improve your type
+                  speed and accuracy.
+                </p>
+              </div>
+              <div className="link-container">
+                <Link to="">GitHub</Link>
+                <Link to="">Deploy</Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
